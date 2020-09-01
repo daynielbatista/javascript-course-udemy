c4be76142e2ca0089fc4d74ad772fb0373fbb2cf
Dayniel <daynielbatista@gmail.com>
2020-09-02 00:06:44 +0100

    Starting JS course1

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*var nombre = prompt('Cual es tu nombre?')
+        document.getElementById('mensaje').innerHTML = `Bienvenido ${nombre}`;
+        console.log('Que tal?');
+        console.table([1,2,3]);
+        console.time('Hola');
+        console.error('Hubo un error');
+        console.warn('Cuidado');
+        console.error('Hubo un error');
+        console.warn('Cuidado');
+        console.error('Hubo un error');
+        console.warn('Cuidado');
+        console.error('Hubo un error');
+        console.warn('Cuidado');
+        console.timeEnd('Hola');*/
+
+console.log('Funciona');
