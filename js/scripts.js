02004c34e29bd1d27ecec6c257b4369cec5a1620
Dayniel <daynielbatista@gmail.com>
2020-09-03 23:14:51 +0100

    Learning JS Basics

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,30 @@
         console.warn('Cuidado');
         console.timeEnd('Hola');*/
 
-console.log('Funciona');
+//console.log('Funciona');
 
 var nombreCliente = 'Pedro'; //camelCase * Este es el que se recomienda en JS Nunca empezar con numeros y no se usan caracteres extranhos.
 var nombre_cliente = 'Juan'; //underscore
 var NombreCliente = 'Day'; //pascal case
+
+//Split va a dividir una cadena de texto
+let nombre;
+let actividad = 'Estoy aprendiendo JS con el curso';
+//nombre = actividad.split(' ');
+//console.log(nombre);
+
+//Reemplazar un valor
+/*let nombre = actividad.replace('JS', 'Javascript');
+console.log(nombre);*/
+
+//Includes revisa que algo exista
+/*let email = 'correo@gmail.com';
+nombre = email.includes('@');
+console.log(nombre);*/
+
+//Repetir un string:
+let master = 'Master ',
+    puppets = 'Of puppets';
+
+console.log (master.repeat(3));
+console.log(puppets);
